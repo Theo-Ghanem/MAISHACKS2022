4ffc1e5e1b253680ec44057b966f750fb3366f89
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-10-01 18:41:19 -0400

    added text input field to frontend

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,37 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 
-export default function FileUpload(){
-	const [selectedFile, setSelectedFile] = useState();
-	const [isSelected, setIsSelected] = useState(false);
+export default function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+  const [jobDescription, setJobDescription] = useState();
 
-	const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsSelected(true);
-	};
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
 
-	const handleSubmission = () => {
-        console.log(selectedFile)
-	};
+  const handleSubmission = () => {
+    console.log(selectedFile);
+  };
 
-	return(
-   <div>
-			<input type="file" name="file" onChange={changeHandler} />
-			{isSelected ? (
-				<div>
-					<p>Filename: {selectedFile.name}</p>
-					<p>Filetype: {selectedFile.type}</p>
-					<p>Size in bytes: {selectedFile.size}</p>
-					<p>
-						lastModifiedDate:{' '}
-						{selectedFile.lastModifiedDate.toLocaleDateString()}
-					</p>
-				</div>
-			) : (
-				<p>Select a file to show details</p>
-			)}
-			<div>
-				<button onClick={handleSubmission}>Submit</button>
-			</div>
-		</div>
-	)
+  return (
+    <div>
+      <TextField
+        onChange={(e) => setJobDescription(e.target.value)}
+        id="outlined-multiline-static"
+        label="Multiline"
+        multiline
+        rows={4}
+        defaultValue="Default Value"
+      />
+
+      <input type="file" name="file" onChange={changeHandler} />
+      {isSelected ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{" "}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
+    </div>
+  );
 }
