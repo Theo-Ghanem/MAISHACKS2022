b5d8726c9858838d34ea053f00c9d2c06a5d35d4
sehr-m <58871345+sehr-m@users.noreply.github.com>
2022-10-02 05:19:27 -0400

    im sleepy :(

--- frontend/src/pages/keywordSelection/index.jsx
+++ frontend/src/pages/keywordSelection/index.jsx
@@ -3,6 +3,9 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import "./Keywords.css";
+import Button from '@mui/material/Button';
+// import Typography from "@material-ui/core/Typography";
+// import { makeStyles } from "@material-ui/styles";
 
 export default function Customize({ goBack, wordList, submitList }) {
   const [newList, setNewList] = useState(wordList);
@@ -25,6 +28,10 @@ export default function Customize({ goBack, wordList, submitList }) {
       addWord(word);
     }
   };
+  // const useStyles = makeStyles(() => ({
+  //   formControlLabel: { fontSize: "30px", "& label": { fontSize: "0.6rem" } }
+  // }));
+
   return (
     <div>
       <button onClick={goBack}>Go Back</button>
@@ -33,13 +40,16 @@ export default function Customize({ goBack, wordList, submitList }) {
         {/* iterate through the keywords of the job description */}
         {wordList.map((word) => (
           <FormControlLabel
+          // style={styles.formControlLabel}
             control={
               <Checkbox
                 checked={newList.includes(word)}
                 onChange={() => toggleWord(word)}
               />
             }
-            label={word}
+            label={word
+          // <Typography className={styles.formControlLabel}>word</Typography>
+        }
           />
         ))}
       </FormGroup>
