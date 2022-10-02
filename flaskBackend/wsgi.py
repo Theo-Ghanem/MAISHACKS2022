34d27701b8a6f913eb4227e2bed95f9855d2c018
ryanreszetnik <rres@rogers.com>
2022-10-02 07:00:29 -0400

    Docker shindigs

--- flaskBackend/wsgi.py
+++ flaskBackend/wsgi.py
@@ -1,7 +1,17 @@
 #wsgi.py
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
 
-from main import app
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home_view():
+        return "<h1>Hello World!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(debug=True, port=33507)
