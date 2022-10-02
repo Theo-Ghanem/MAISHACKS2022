95200e7782143dd3813e0525e9913b64863fbf87
ryanreszetnik <rres@rogers.com>
2022-10-01 22:15:13 -0400

    cors

--- backend/gettingstarted/settings.py
+++ backend/gettingstarted/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "hello.middleware.corsMiddleware",
 ]
 
 ROOT_URLCONF = "gettingstarted.urls"
