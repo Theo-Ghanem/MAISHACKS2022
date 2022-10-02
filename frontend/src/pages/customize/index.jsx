7a8b156ce2db46b27b31f5510f1eafd592811fc0
Theo Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-10-02 03:49:49 -0400

    Added lists of keywords from job description on second page :airplane:

--- frontend/src/pages/customize/index.jsx
+++ frontend/src/pages/customize/index.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import "./Keywords.css";
 
 export default function Customize({ goBack, resume, description }) {
   const print = () => {
@@ -8,6 +12,13 @@ export default function Customize({ goBack, resume, description }) {
     <div>
       <button onClick={goBack}>Go Back</button>
       <button onClick={print}>Print</button>
+      <FormGroup>
+        {/* need to be able to iterate through the keywords of the job description */}
+        <FormControlLabel control={<Checkbox />} label="banana" />
+        <FormControlLabel control={<Checkbox />} label="potato" />
+        <FormControlLabel control={<Checkbox />} label="burger" />
+        <FormControlLabel control={<Checkbox />} label="pineapple" />
+      </FormGroup>
     </div>
   );
 }
