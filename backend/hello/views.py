fcfed0a4f30dc66b68b1cd11eee25dc3c17cfebc
ryanreszetnik <rres@rogers.com>
2022-10-01 22:29:24 -0400

    cors

--- backend/hello/views.py
+++ backend/hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-from utils.parseDocx import parseDocx
+from .utils.parseDocx import parseDocx
 # Create your views here.
 def index(request):
     return HttpResponse('Hello from Python!')
